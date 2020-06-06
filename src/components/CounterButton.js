751496f23c37de774e26f5415eeed033025b9d35
nikicivan <ivan.nikic@gmail.com>
2020-06-06 02:46:28 +0200

    update testing files

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -18,10 +18,9 @@ class CounterButton extends Component {
 			return {count: state.count + 1}
 		})
 	}
-	render() {
-		console.log('CounterButton');
+	render() {		
 		return (
-			<button color={this.props.color} onClick={this.updateCount}>
+			<button id="counter" color={this.props.color} onClick={this.updateCount}>
 				Count: {this.state.count}
 			</button>
 		);
--- src/reducers.js
+++ src/reducers.js
@@ -17,7 +17,7 @@ export const searchRobots = (state=initialStateSearch, action={}) => {
 const initialStateRobots = {
 	isPending: false,
 	robots: [],
-	error: ''
+	//error: ''
 }
 
 export const requestRobots = (state=initialStateRobots, action={}) => {
